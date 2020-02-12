fd392f00d683a01bc4038c4c8470f48317768dbc
KershawK5646 <43454481+KershawK5646@users.noreply.github.com>
2020-02-11 21:20:11 -0500

    General login page
    Contains general login page using Flask and Bootstrap templates.

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 Login Page
 '''
 # imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # Create the application object
 app = Flask(__name__)
@@ -16,6 +16,17 @@ def home():
 def welcome():
     return render_template('welcome.html') # render a template
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
 # Start the server with the run method
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
