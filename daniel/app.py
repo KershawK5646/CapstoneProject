d94e3d991c0fca855643eba7e4489107f39833aa
biglerd7304 <43176266+biglerd7304@users.noreply.github.com>
2020-02-24 15:11:44 -0500

    .
    following tutorials

--- daniel/app.py
+++ daniel/app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template
+# inport the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
 
+# create the application abject
 app = Flask(__name__)
 
-
+# user decorators to link the function to a url
 @app.route('/')
 def home():
-	return "Hello, world!"
-
+	return "Hello, world!"	# return a string
 
 @app.route('/welcome')
 def welcome():
-	return render_template("welcome.html")
+	return render_template("welcome.html")	# render a template
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid credentials. Please try again.'
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
 
 
+
+# start the server with the 'run()' method
 if __name__ == '__main__':
 	app.run(debug=True)
