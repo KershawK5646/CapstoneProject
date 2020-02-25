3fb7158b396a7f3ea387a97394e197ee62829b87
KershawK5646 <43454481+KershawK5646@users.noreply.github.com>
2020-02-24 22:00:00 -0500

    Minor tweaks

--- sql.py
+++ sql.py
@@ -2,7 +2,8 @@ import sqlite3
 
 with sqlite3.connect("sample.db") as connection:
     c = connection.cursor()
-    c.execute("DROP TABLE posts")
+    # TODO Delete the drop table command
+    #c.execute("DROP TABLE posts")
     c.execute("CREATE TABLE posts(title Text, description TEXT)")
-    c.execute('INSERT INTO posts VALUES("Good", "I\', good.")')
-    c.execute('INSERT INTO posts VALUES("Well", "I\', well.")')
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m, good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m, well.")')
