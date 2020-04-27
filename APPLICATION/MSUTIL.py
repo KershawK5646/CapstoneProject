11b50ae6a0d4b86189c427007456cfad30bd2636
Kyler Kershaw <KershawK5646@student.faytechcc.edu>
2020-04-26 20:35:15 -0400

    Update to processing input.
    Created method to delete input special characters that could interfere with queries.
    Created page and system to view currently booked inventory.
    Created page and system to view currently booked rooms.

--- APPLICATION/MSUTIL.py
+++ APPLICATION/MSUTIL.py
@@ -6,5 +6,32 @@ def stripSingleQuotes(word):
     word = word.strip("'")
     return word
 
+def stripSpecialCharacters(word):
+    word = word.replace("*", "")
+    word = word.replace(";", "")
+    word = word.replace(":", "")
+    word = word.replace("\\", "")
+    word = word.replace("/", "")
+    word = word.replace('"', '')
+    word = word.replace("<", '')
+    word = word.replace(",", '')
+    word = word.replace(">", '')
+    word = word.replace("?", '')
+    word = word.replace("[", '')
+    word = word.replace("{", '')
+    word = word.replace("]", '')
+    word = word.replace("}", '')
+    word = word.replace("|", '')
+    word = word.replace("=", '')
+    word = word.replace("+", '')
+    word = word.replace(")", '')
+    word = word.replace("(", '')
+    word = word.replace("*", '')
+    word = word.replace("_", '')
+    word = word.replace("-", '')
+    return word
+
+
+
 def debugFormat():
     print("==========================================")
